d761920c296f7ad46bcad4f598b22c743ab85ea6
caoyanping <caoyp2@lenovo.com>
2025-03-21 11:18:55 +0800

    加入路由

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { echarts } from '@/utils/echartsPlugin.js'
 import '@/assets/style/variables.less'
+import router from './router/routes.js'
 
 const app = createApp(App);
 app.provide('echarts', echarts)
+app.use(router)
 app.mount('#app');
